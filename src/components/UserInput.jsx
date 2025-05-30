c23f7e929c0f477093728f41a9f739affb2f928b
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2025-05-30 20:08:36 +0300

    Add styled UserInput component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+
 export default function UserInput() {
-    return <div id="user-input">
-      <input></input>
-  </div>;
+  const [initialInvestment, setInitialInvestment] = useState(10000);
+  const [annualInvestment, setAnnualInvestment] = useState(1200);
+  const [expectedReturn, setExpectedReturn] = useState(6);
+  const [duration, setDuration] = useState(10);
+
+  return (
+    <div id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={initialInvestment}
+            onChange={(event) => setInitialInvestment(event.target.value)}
+            required
+          ></input>
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            value={annualInvestment}
+            onChange={(event) => setAnnualInvestment(event.target.value)}
+            required
+          ></input>
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            value={expectedReturn}
+            onChange={(event) => setExpectedReturn(event.target.value)}
+            required
+          ></input>
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            value={duration}
+            onChange={(event) => setDuration(event.target.value)}
+            required
+          ></input>
+        </p>
+      </div>
+    </div>
+  );
 }
